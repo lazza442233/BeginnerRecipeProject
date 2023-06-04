f4c11a54044bede8df48ce56804c05b5df3cb07d
Lennon Abonyi <l.abonyi@bettube.com>
2023-06-04 18:05:16 +1000

    Add project files.

---
+++ RecipeProject/Models/Ingredient.cs
@@ -0,0 +1,11 @@
+﻿namespace RecipeProject.Models
+{
+    public class Ingredient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string UnitOfMeasurement { get; set; } = string.Empty;
+        public string? Notes { get; set; }
+    }
+
+}
---
+++ RecipeProject/Models/Recipe.cs
@@ -0,0 +1,11 @@
+﻿namespace RecipeProject.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public List<Ingredient> Ingredients { get;}
+
+    }
+}
